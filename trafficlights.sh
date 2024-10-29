ae1b28095f3ea2022589903c9adeb44e7bb59fb6
Aryu54 <72098871+Aryu54@users.noreply.github.com>
2024-10-28 18:08:10 -0700

    Update trafficlights.sh

--- trafficlights.sh
+++ trafficlights.sh
@@ -61,7 +61,17 @@ setOutput $GREEN
 
 while [ 1 ]
 do
-  #exit 0
+  files=$(ls *.mp3)
+  echo files
+  for i in "${!files[@]}"; do
+    j=$(( RANDOM % "${#files[@]}" ))
+    [ "$i" -ne "$j" ] && {
+      tmp="${files[i]}"
+      files[i]="${files[j]}"
+      files[j]="$tmp"
+    }
+  done
+  echo files
   state=$(getValue $GREEN | awk '{print $5}')
   if [[ "$state" == "lo" ]]; then
     echo MUSIC
